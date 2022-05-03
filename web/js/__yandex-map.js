e62d941618be909c303a790e99993830615fcaea
Дмитрий <dkrech07@gmail.com>
2022-05-03 10:11:58 +0500

    Вносит правки в фильтрацию на фронте

--- web/js/__yandex-map.js
+++ web/js/__yandex-map.js
@@ -8,9 +8,7 @@ if(!yandexMap.dataset.latitude && !yandexMap.dataset.longitude) {
 ymaps.ready(init);
 
 function init () {
-    var myMap = new ymaps.Map('map', {
-            // center: [55.76, 37.64],
-        
+    var myMap = new ymaps.Map('map', {    
             center: [yandexMap.dataset.latitude, yandexMap.dataset.longitude],
             zoom: 5,
             controls: []
@@ -32,15 +30,9 @@ function init () {
     // обратимся к дочерним коллекциям ObjectManager.
     objectManager.objects.options.set('preset', 'islands#greenDotIcon');
     objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
-    // myMap.geoObjects.removeAll(); 
-    myMap.geoObjects.add(objectManager);
 
-    $.ajax({
-        url: '/checkbox' // "/ajax" // '/checkbox' "http://localhost/wimc/web/checkbox"
-    }).done(function(data) {
-        objectManager.add(data);
-    });
-
+    // Отрисовывает точки при поиске обеъкта на карте;
+    
     $('#search-customs').on('beforeSubmit', function(){
         var data = $(this).serialize();
         $.ajax({
@@ -155,6 +147,45 @@ function init () {
         return false;
     });
 
+    // Отприсовывает точки при загрузке страницы;
 
-    
+    $.ajax({
+        url: '/checkbox' // "/ajax" // '/checkbox' "http://localhost/wimc/web/checkbox"
+    }).done(function(response) {
+        objectManager.add(response);
+        console.log(response);
+    });
+   
+
+    // Отрысовывает точки при фильтрации по типам постов;
+
+    let checkboxes = Array.from(document.querySelectorAll('.customs-checkbox'));
+
+    var data = {};
+    checkboxes.forEach(function(checkbox, i) {
+        checkbox.onchange = function() {
+            checkboxes.forEach(function(checkbox){
+                data[checkbox.id] = checkbox.checked ? 1:0;
+            });
+
+            $.ajax({
+                url: '/checkbox', // '/checkbox' 'http://localhost/wimc/web/checkbox'
+                type: 'POST',
+                data: data,
+                success: function (response) {
+                    objectManager.removeAll();
+
+                    objectManager.add(response);
+
+                    console.log(response);
+                }
+            });
+        }
+    });
+
+ 
+
+    myMap.geoObjects.add(objectManager);
+
+
 }
