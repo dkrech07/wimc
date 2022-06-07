0ceea5f486212b52e1593caf5bb0161e894d2722
Дмитрий Кречетников <dkrech07@gmail.com>
2022-06-07 10:01:11 +0500

    вносит правки

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=',
-    'username' => 'root',
+    'username' => '',
     'password' => '',
     'charset' => 'utf8',
 
