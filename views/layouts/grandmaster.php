df4e1097f94d23091dbde9509f60a89e3d5739d1
Дмитрий <dkrech07@gmail.com>
2022-06-12 00:05:56 +0500

    Вносит правки

--- views/layouts/grandmaster.php
+++ views/layouts/grandmaster.php
@@ -115,7 +115,7 @@ if (isset(Yii::$app->user->identity->login)) {
 
     <footer class="footer mt-auto py-3 text-muted">
         <div class="container">
-            <p class="float-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="float-left">&copy; MyCustoms Grandmaster <?= date('Y') ?></p>
             <p class="float-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
