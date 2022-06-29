9fa80c350d9d1037ba3b7567c55e5851d146adf1
Дмитрий Кречетников <dkrech07@gmail.com>
2022-06-29 12:12:03 +0500

    Выносит функционал фильтрации в сервисную функцию

--- web/js/yandex-map.js
+++ web/js/yandex-map.js
@@ -50,6 +50,9 @@ function init() {
             'excise': 0,
             'others': 0,
             'captions': 0,
+            'autocomplete': null,
+            'latitude': null,
+            'longitude': null,
         };
 
         // Карта цветов меток;
@@ -59,7 +62,6 @@ function init() {
             'excise': '#0000FF',
             'others': '#E8B000',
         };
-
 
     // Отрисовываю основные таможенные посты;
     $.ajax({
@@ -101,7 +103,7 @@ function init() {
                 data: data,
                 success: function (response) {
                     let customsCoords = JSON.parse(response);
-    
+
                     if (data[checkbox.id] == 1 && checkbox.id !== 'captions') {
     
                         window.points.getPoints(geoObjects[checkbox.id], customsCoords[checkbox.id], pointsColors[checkbox.id], data['captions']);
@@ -143,7 +145,121 @@ function init() {
     window.points.checkClusterPoints(myMap, clusterer);
     window.points.zoomOut(myMap, clusterer);
 
+        // Отрисовывает точки при поиске обеъкта на карте;
+        $('#search-customs').on('beforeSubmit', function () {
+            data['autocomplete'] = this['SearchCustoms[autocomplete]'].value;
+            data['latitude'] = this['SearchCustoms[latitude]'].value;
+            data['longitude'] = this['SearchCustoms[longitude]'].value;
+
+            $.ajax({
+                url: '/search',
+                type: 'POST',
+                data: data,
+                success: function (response) {
+                    let searchData = JSON.parse(response);
     
+                    // console.log('вывожу в консоль результат поиска точки'); // вывожу в консоль результат поиска точки
+                    // console.log(searchData); // вывожу в консоль результат поиска точки
+    
+                    var searchInputElement = document.querySelector('#autocomplete');
+    
+                    if (!searchData) {
+                        searchInputElement.classList.remove('input-shadow');
+                        searchInputElement.classList.add('input-alert');
+    
+                        var searchListElement = document.querySelector('#ui-id-1');
+                        searchListElement.style.width = searchInputElement.offsetWidth + 'px';
+                        searchListElement.style.top = $('#autocomplete').offset()['top'] + 36 + 'px';
+                        searchListElement.style.left = '50%';
+                        searchListElement.style.marginLeft = -searchInputElement.offsetWidth / 2 + 'px';
+    
+                        searchListElement.style.display = 'block';
+                        while (searchListElement.firstChild) {
+                            searchListElement.removeChild(searchListElement.firstChild);
+                        }
+                        var noResultsElement = document.createElement('li');
+                        noResultsElement.className = "no-results-item"; //ui-menu-item
+                        noResultsElement.innerHTML = "<span tabindex='-1' class='no-results ui-menu-item-wrapper'><i style='color: #6c757d'>Ничего не нашлось... Попробуйте изменить или дополнить запрос</i></span>";
+                        searchListElement.append(noResultsElement);
+    
+                        document.addEventListener('click', evt => {
+                            if (searchListElement && evt.target.id !== 'autocomplete') {
+                                searchListElement.style.display = 'none';
+                            }
+                        });
+                        return;
+                    } else {
+                        if (searchInputElement.classList.contains('input-alert')) {
+                            searchInputElement.classList.remove('input-alert');
+                            searchInputElement.classList.add('input-shadow');
+                        }
+                    }
+    
+                    function getNearestInfo(item) {
+                        var nearestItem = document.createElement('li');
+                        nearestItem.className = 'nearest-item';
+    
+                        var nearestItemDistance = document.createElement('span');
+                        nearestItemDistance.className = 'nearest-distance';
+                        nearestItemDistance.textContent = '~' + Math.floor(item['distance'] * 100000) + ' км';
+    
+                        var nearestItemName = document.createElement('span');
+                        nearestItemName.className = 'nearest-name';
+                        nearestItemName.textContent = item['namt'];
+    
+                        var nearestItemAddress = document.createElement('span');
+                        nearestItemAddress.className = 'nearest-address';
+                        nearestItemAddress.textContent = item['adrtam'];
+    
+                        nearestItem.append(nearestItemDistance);
+                        nearestItem.append(nearestItemName);
+                        nearestItem.append(nearestItemAddress);
+    
+                        return nearestItem;
+                    }
+    
+                    var nearestPopupElement = document.querySelector('.nearest-popup');
+                    if (nearestPopupElement.classList.contains('nearest-active')) {
+                        var nearestLists = nearestPopupElement.querySelectorAll('.nearest-list');
+                        while (nearestLists[0].firstChild) {
+                            nearestLists[0].removeChild(nearestLists[0].firstChild);
+                        }
+                        while (nearestLists[1].firstChild) {
+                            nearestLists[1].removeChild(nearestLists[1].firstChild);
+                        }
+                    } else {
+                        nearestPopupElement.classList.remove('nearest-disabled');
+                        nearestPopupElement.classList.add('nearest-active');
+                    }
+    
+                    if (nearestPopupElement.classList.contains('roll-down')) {
+                        var nearestTitles = nearestPopupElement.querySelectorAll('.nearest-title');
+                        var nearestLists = nearestPopupElement.querySelectorAll('.nearest-list');
+                        var rollButtonElement = nearestPopupElement.querySelector('.roll-button');
+    
+                        nearestPopupElement.classList.remove('roll-down');
+                        nearestPopupElement.classList.add('roll-up');
+    
+                        rollButtonElement.textContent = 'Развернуть';
+    
+                        nearestTitles.forEach(function (element) {
+                            element.style.display = 'block';
+                        });
+    
+                        nearestLists.forEach(function (element) {
+                            element.style.display = 'block';
+                        });
+                    }
+    
+                    var nearestContainerElement = nearestPopupElement.querySelector('.nearest-list');
+                    var otherContainerElement = nearestPopupElement.querySelector('.nearest-others');
+                    nearestContainerElement.append(getNearestInfo(searchData['nearest_points'][0]));
+                    otherContainerElement.append(getNearestInfo(searchData['nearest_points'][1]));
+                    otherContainerElement.append(getNearestInfo(searchData['nearest_points'][2]));
+                }
+            });
+            return false;
+        });
     
  
 }
